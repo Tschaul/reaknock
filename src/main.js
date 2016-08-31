050b02f071b1a61e1c37170682884a8eeda1d102
Julian Steinwachs <julian.steinwachs@fau.de>
2016-08-31 20:02:21 +0200

    abstract class KnockoutView

--- src/main.js
+++ src/main.js
@@ -7,31 +7,104 @@ var __extends = (this && this.__extends) || function (d, b) {
 var ko = require('knockout');
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Priority;
+(function (Priority) {
+    Priority[Priority["Low"] = 0] = "Low";
+    Priority[Priority["Mid"] = 1] = "Mid";
+    Priority[Priority["High"] = 2] = "High";
+})(Priority || (Priority = {}));
+var ItemViewModel = (function () {
+    function ItemViewModel(data) {
+        var _this = this;
+        this.name = ko.observable();
+        this.done = ko.observable();
+        this.priority = ko.observable();
+        this.toggle = function () {
+            _this.done(!_this.done());
+        };
+        this.name(data.name);
+        this.done(data.done);
+        this.priority(data.priority);
+    }
+    return ItemViewModel;
+}());
 var ViewModel = (function () {
     function ViewModel(data) {
-        this.name = ko.observable();
-        this.name(data.name);
+        var _this = this;
+        this.title = ko.observable();
+        this.todos = ko.observableArray();
+        this.addTodo = function () {
+            _this.todos.push(new ItemViewModel({
+                name: "Test",
+                done: false,
+                priority: Priority.Low
+            }));
+        };
+        this.title(data.name);
+        this.todos.push(new ItemViewModel({
+            name: "Kaffee",
+            done: false,
+            priority: Priority.Low,
+        }));
+        this.todos.push(new ItemViewModel({
+            name: "Milch",
+            done: true,
+            priority: Priority.Mid,
+        }));
+        this.todos.push(new ItemViewModel({
+            name: "Zucker",
+            done: false,
+            priority: Priority.High,
+        }));
     }
     return ViewModel;
 }());
-var View = (function (_super) {
-    __extends(View, _super);
-    function View(props) {
+var KnockoutView = (function (_super) {
+    __extends(KnockoutView, _super);
+    function KnockoutView(props) {
         var _this = this;
         _super.call(this, props);
-        this.fuzzName = function () {
-            _this.props.model.name(_this.props.model.name() + "fuzz");
-        };
-        this.computedRender = ko.computed(function () {
-            return (React.createElement("div", null, React.createElement("span", null, _this.props.model.name()), React.createElement("button", {onClick: _this.fuzzName}, "fuzz")));
-        }, this);
-        this.computedRender.subscribe(function () { return _this.forceUpdate(); });
+        this.trigger = ko.observable();
+        this.renderComputed = ko.computed(function () {
+            _this.trigger();
+            return _this.renderModel(_this.props.model);
+        });
+        this.renderComputed.subscribe(function () { return _this.forceUpdate(); });
     }
-    View.prototype.render = function () {
-        return this.computedRender();
+    KnockoutView.prototype.componentWillReceiveProps = function () {
+        this.trigger.notifySubscribers();
+    };
+    KnockoutView.prototype.render = function () {
+        return this.renderComputed();
+    };
+    return KnockoutView;
+}(React.Component));
+var View = (function (_super) {
+    __extends(View, _super);
+    function View() {
+        _super.apply(this, arguments);
+    }
+    View.prototype.renderModel = function (model) {
+        var items = model.todos().map(function (item, n) { return (React.createElement(ItemView, {model: item, key: n})); });
+        return (React.createElement("div", null, React.createElement("h2", null, model.title()), React.createElement("ul", null, items), React.createElement("button", {onClick: model.addTodo}, "Add")));
     };
     return View;
-}(React.Component));
+}(KnockoutView));
+var ItemView = (function (_super) {
+    __extends(ItemView, _super);
+    function ItemView() {
+        _super.apply(this, arguments);
+    }
+    ItemView.prototype.renderModel = function (model) {
+        if (model.done()) {
+            return React.createElement("li", {onClick: model.toggle, style: { textDecoration: "line-through" }}, model.name());
+        }
+        else {
+            return React.createElement("li", {onClick: model.toggle}, model.name());
+        }
+    };
+    return ItemView;
+}(KnockoutView));
 var vm = new ViewModel({ name: "test" });
 ReactDOM.render(React.createElement(View, {model: vm}), document.getElementById("view"));
 //# sourceMappingURL=main.js.map
--- src/main.tsx
+++ src/main.tsx
@@ -2,41 +2,115 @@ import * as ko from 'knockout'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-class ViewModel {
+enum Priority {
+    Low, Mid, High
+}
+
+class ItemViewModel {
+
     constructor(data){
         this.name(data.name);
+        this.done(data.done);
+        this.priority(data.priority);
     }
 
-    public name = ko.observable<string>();
+    public name  = ko.observable<string>();
+    public done = ko.observable<boolean>();
+    public priority = ko.observable<Priority>();
+
+    public toggle = () => {
+        this.done(!this.done());
+    }
+}
+
+class ViewModel {
+    constructor(data){
+        this.title(data.name);
+        this.todos.push(new ItemViewModel({
+            name: "Kaffee",
+            done: false,
+            priority: Priority.Low,
+        }))
+        this.todos.push(new ItemViewModel({
+            name: "Milch",
+            done: true,
+            priority: Priority.Mid,
+        }))
+        this.todos.push(new ItemViewModel({
+            name: "Zucker",
+            done: false,
+            priority: Priority.High,
+        }))
+    }
+
+    public title = ko.observable<string>();
+    
+    public todos = ko.observableArray<ItemViewModel>();
+
+    public addTodo = () => {
+        this.todos.push(new ItemViewModel({
+            name: "Test",
+            done: false,
+            priority: Priority.Low
+        }))
+    }
 
 }
 
 
-class View extends React.Component<{model:ViewModel},{}> {
-
-    constructor(props){
+abstract class KnockoutView<TModel> extends React.Component<{model:TModel},{}>{
+    constructor(props: {model:TModel}){
         super(props);
-        this.computedRender.subscribe(()=>this.forceUpdate())
+
+        this.renderComputed.subscribe(()=>this.forceUpdate())
     }
 
-    public fuzzName = () => {
-        this.props.model.name(this.props.model.name()+"fuzz");
+    public abstract renderModel(model: TModel);
+
+    private trigger = ko.observable();
+
+    private renderComputed = ko.computed(()=>{
+        this.trigger();
+        return this.renderModel(this.props.model)
+    })
+
+    componentWillReceiveProps(){
+        this.trigger.notifySubscribers();
     }
 
-    computedRender = ko.computed(()=>{
+    render(){
+        return this.renderComputed();
+    }
+
+}
+
+class View extends KnockoutView<ViewModel> {
+    renderModel(model){
+        let items = model.todos().map((item,n)=>(
+            <ItemView model={item} key={n}/>
+        ))
+
         return (
             <div>
-                <span>{this.props.model.name()}</span>
-                <button onClick={this.fuzzName}>fuzz</button>
+                <h2>{model.title()}</h2>
+                <ul>
+                    {items}
+                </ul>
+                <button onClick={model.addTodo}>Add</button>
             </div>
         )
-    },this)
-
-    render() {
-        return this.computedRender();
     }
 }
 
+class ItemView extends KnockoutView<ItemViewModel> {
+    renderModel(model:ItemViewModel){
+        if(model.done()){
+            return <li onClick={model.toggle} style={{textDecoration:"line-through"}}>{model.name()}</li>
+        }else{
+            return <li onClick={model.toggle}>{model.name()}</li>
+        }
+    }
+}
 
 var vm = new ViewModel({name:"test"})
 
